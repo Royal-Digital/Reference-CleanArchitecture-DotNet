3768a5ff672bc73e796e616407f72952273d2a6e
Travis Frisinger <tmfrisinger@gmail.com>
2017-05-22 22:29:06 +0200

    Added CalculateChangeUseCase

---
+++ source/HeavyMetal.BakeSale.Domain/TOs/CalculateChangeInputTo.cs
@@ -0,0 +1,8 @@
+namespace HeavyMetal.BakeSale.Domain.TOs
+{
+    public class CalculateChangeInputTo
+    {
+        public double Total { get; set; }
+        public double Payment { get; set; }
+    }
+}
---
+++ source/HeavyMetal.BakeSale.Domain/UseCases/ICalculateChangeUseCase.cs
@@ -0,0 +1,9 @@
+﻿using HeavyMetal.BakeSale.Domain.TOs;
+using TddBuddy.CleanArchitecture.Domain;
+
+namespace HeavyMetal.BakeSale.Domain.UseCases
+{
+    public interface ICalculateChangeUseCase : IUseCase<CalculateChangeInputTo,double>
+    {
+    }
+}
