14a56f17ca7472ec5f907a337e89f90dde26dfd3
T-rav <tmfrisinger@gmail.com>
2017-08-08 21:54:13 +0200

    removed GetHashCode

--- source/Todo.Domain/Model/TodoItemModel.cs
+++ source/Todo.Domain/Model/TodoItemModel.cs
@@ -2,7 +2,9 @@
 
 namespace Todo.Domain.Model
 {
+#pragma warning disable CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
     public class TodoItemModel
+#pragma warning restore CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
     {
         public Guid Id { get; set; }
         public string ItemDescription { get; set; }
@@ -19,14 +21,11 @@ namespace Todo.Domain.Model
             return DateTime.Now.CompareTo(CompletionDate) >= 0;
         }
 
+#pragma warning disable 659
         public override bool Equals(object obj)
+#pragma warning restore 659
         {
             return Id != Guid.Empty && Id == (obj as TodoItemModel).Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
         }
     }
 }
