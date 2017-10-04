7f9f9cd5c6c9c4a9c36cce08dd4cdf89ea4fa2fb
T-rav <travis.frisinger@chillisoft.co.za>
2017-10-04 16:13:48 +0200

    Added dependency on WebApi.RemoveControllerSuffix so I do not need to suffix my controllers

--- source/Todo.Api/App_Start/WebApiConfig.cs
+++ source/Todo.Api/App_Start/WebApiConfig.cs
@@ -1,7 +1,6 @@
 ﻿using System.Net.Http.Headers;
-using System.Reflection;
 using System.Web.Http;
-using System.Web.Http.Dispatcher;
+using WebApi.ControllerSuffixRemover;
 
 namespace Todo.Api
 {
@@ -9,8 +8,7 @@ namespace Todo.Api
     {
         public static void Register(HttpConfiguration config)
         {
-            OverrideControllerTypeResolver(config);
-            OverrideControllerConstant();
+            config.AllowControllersToNotNeedControllerSuffix();
 
             MapRoutes(config);
 
@@ -20,18 +18,6 @@ namespace Todo.Api
         private static void ConfigureMediaType(HttpConfiguration config)
         {
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
-        }
-
-        private static void OverrideControllerConstant()
-        {
-            var suffix =
-                typeof(DefaultHttpControllerSelector).GetField("ControllerSuffix", BindingFlags.Static | BindingFlags.Public);
-            if (suffix != null) suffix.SetValue(null, string.Empty);
-        }
-
-        private static void OverrideControllerTypeResolver(HttpConfiguration config)
-        {
-            config.Services.Replace(typeof(IHttpControllerTypeResolver), new CustomHttpControllerTypeResolver());
         }
 
         private static void MapRoutes(HttpConfiguration config)
