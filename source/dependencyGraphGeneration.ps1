9b3fe296dbbed27c974152f110e8eeb4e8bca8f9
T-rav <tmfrisinger@gmail.com>
2017-11-01 16:08:21 +0200

    Got a working solution for .netcore, .netstandard and .netframework

--- source/dependencyGraphGeneration.ps1
+++ source/dependencyGraphGeneration.ps1
@@ -5,6 +5,7 @@ function Get-ProjectReferences
         [string]$rootFolder,
         [string[]]$excludeProjectsContaining
     )
+	
     dir $rootFolder -Filter *.csproj -Recurse |
         # Exclude any files matching our rules
         where { "*$($_.Name)*" -notlike $excludeProjectsContaining } |
@@ -19,18 +20,36 @@ function Select-References
     )
     process
     {
-        $projectName = $_.BaseName
-        [xml]$projectXml = Get-Content $_.FullName
-        $ns = @{ defaultNamespace = "http://schemas.microsoft.com/developer/msbuild/2003" }
-        $projectXml |
-            # Find the references xml nodes
-            Select-Xml '//defaultNamespace:ProjectReference/defaultNamespace:Name' -Namespace $ns |
-            # Get the node values
-            foreach { $_.node.InnerText } |
-            # Exclude any references pointing to projects that match our rules
-            where { $excludeProjectsContaining -notlike "*$_*" } |
-            # Output in yuml.me format
-            foreach { "[" + $projectName + "] -> [" + $_ + "]" }
+		$baseProjectName = $_.BaseName
+		[System.Xml.XmlDocument]$file = new-object System.Xml.XmlDocument
+		$file.load($_.FullName)
+		$projectReferences = $file.SelectNodes("/Project/ItemGroup/ProjectReference")
+		# .NetCore and .NetStandard
+		if ( $projectReference.length -gt 0 ){
+			foreach ($projectReference in $projectReferences) {
+			  $reference = $projectReference.Include
+			  $parts = $reference.split("\");
+			  $projectNameIndex = $parts.length - 1;
+			  $projectName = $parts[$projectNameIndex];
+			  $projectNameLength = $lastPart.length;
+			  $projectReference = $projectName.Replace(".csproj","")
+			  "[" + $baseProjectName + "] -> [" + $projectReference + "]"
+			}
+		}else{ # .NetFramework
+			Write-Host "foo"
+			$projectName = $_.BaseName
+			[xml]$projectXml = Get-Content $_.FullName
+			$ns = @{ defaultNamespace = "http://schemas.microsoft.com/developer/msbuild/2003" }
+			$projectXml |
+				# Find the references xml nodes
+				Select-Xml '//defaultNamespace:ProjectReference/defaultNamespace:Name' -Namespace $ns |
+				# Get the node values
+				foreach { $_.node.InnerText } |
+				# Exclude any references pointing to projects that match our rules
+				where { $excludeProjectsContaining -notlike "*$_*" } |
+				# Output in yuml.me format
+				foreach { "[" + $projectName + "] -> [" + $_ + "]" }
+		}
     }
 }
 
